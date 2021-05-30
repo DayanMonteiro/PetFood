63290d15c34c8d2af24859c39d0ddd5beba027f1
DayanMonteiro <dmsmamb@gmail.com>
2021-05-29 21:06:41 -0300

    iniciando pagina de checkout

---
+++ web/src/components/product/list/index.js
@@ -0,0 +1,15 @@
+
+
+const Product = () => {
+    
+    return (
+        <div className="col-12">
+            <div className="col-3"></div>
+            <div className="col-6"></div>
+            <div className="col-3"></div>
+        </div>
+
+    )
+}
+
+export default Product;
