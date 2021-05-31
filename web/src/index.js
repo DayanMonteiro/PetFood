8c879ac189e2899be7c14e5225c388243707b702
Dayan Monteiro <dmsmamb@gmail.com>
2021-05-31 18:15:53 -0300

    iniciando home

--- web/src/index.js
+++ web/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './styles/global.css';
+import Home from './pages/home';
 import Sidebar from './components/sidebar';
 import Cadastro from './pages/cadastro';
 import Checkout from './pages/checkout';
@@ -11,7 +12,7 @@ import Petshop from './pages/petshop';
 ReactDOM.render(
   <React.StrictMode>
     <Sidebar />
-    <Petshop />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
